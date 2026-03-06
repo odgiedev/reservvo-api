3717e03bce29a23798dba22984b24c1fb083994e
Odgiedev <odgiedev@gmail.com>
2026-03-06 06:20:00 -0300

    feat: implement aws ses client

---
+++ src/main/java/com/odgiedev/reservvo/config/AwsSesConfig.java
@@ -0,0 +1,32 @@
+package com.odgiedev.reservvo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.secret-access-key}")
+    private String secretAccessKey;
+
+    @Bean
+    public SesClient sesClient() {
+        return SesClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                ))
+                .build();
+    }
+}
