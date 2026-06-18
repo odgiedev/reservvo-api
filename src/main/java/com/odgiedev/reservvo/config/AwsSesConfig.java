77e1df2d02d964b7855a9a911e0b3b55ef8424ea
Odgiedev <odgiedev@gmail.com>
2026-06-18 19:34:40 -0300

    fix: SES client

--- src/main/java/com/odgiedev/reservvo/config/AwsSesConfig.java
+++ src/main/java/com/odgiedev/reservvo/config/AwsSesConfig.java
@@ -3,6 +3,7 @@ package com.odgiedev.reservvo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -14,19 +15,24 @@ public class AwsSesConfig {
     @Value("${aws.region}")
     private String region;
 
-    @Value("${aws.access-key-id}")
+    @Value("${aws.access-key-id:}")
     private String accessKeyId;
 
-    @Value("${aws.secret-access-key}")
+    @Value("${aws.secret-access-key:}")
     private String secretAccessKey;
 
     @Bean
     public SesClient sesClient() {
-        return SesClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
-                ))
-                .build();
+        var builder = SesClient.builder().region(Region.of(region));
+
+        if (accessKeyId.isBlank() || secretAccessKey.isBlank()) {
+            builder.credentialsProvider(AnonymousCredentialsProvider.create());
+        } else {
+            builder.credentialsProvider(StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+            ));
+        }
+
+        return builder.build();
     }
 }
